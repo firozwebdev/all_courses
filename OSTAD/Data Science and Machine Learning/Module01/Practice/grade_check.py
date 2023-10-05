1fa764a4bd2cc6ebf7808eb1ff1215fe4901a3e1
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-10-05 18:58:09 +0600

    data science file added

---
+++ OSTAD/Data Science and Machine Learning/Module01/Practice/grade_check.py
@@ -0,0 +1,16 @@
+# x = int(input("Enter the student's score: "))
+x = 52
+if x>=90:
+    print("A+")
+elif x>=81:
+    print("A")
+elif x>= 70:
+    print("B")
+elif x>=60:
+    print("B-")
+elif x>=50:
+    print ("C")
+elif x>=40:
+    print("D")
+else:
+    print ("x is not present")
