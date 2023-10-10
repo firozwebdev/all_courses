7ffb66b72795a3357f7dd8e128ab0123fa88ca0e
Sabuz <everythingkst@gmail.com>
2023-10-10 12:57:13 +0600

    quiz added to data science module04

---
+++ OSTAD/Data Science and Machine Learning/Module02/quiz.py
@@ -0,0 +1,25 @@
+# salary = 8000
+#
+#
+# def printSalary():
+#     salary = 12000
+#
+#     print("Salary:", salary)
+#
+#
+# printSalary()
+
+# print("Salary:", salary)
+# def fun1(name, age=20):
+#     print(name, age)
+#
+#
+# fun1('Emma')
+
+# aList = [4, 8, 12, 16]
+#
+# aList[1:4] = [20, 24, 28]
+#
+# print(aList)
+# for num in range(-2, -5, -1):
+#     print(num, end=", ")
