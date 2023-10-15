9f5418b61043b8ce81e9c76f4b85c0fb68fe4751
Sabuz <everythingkst@gmail.com>
2023-10-15 14:45:55 +0600

    file  modified

--- OSTAD/php and laravel/module05-Mastering on PHP File, Session, Date, Exception/practice/01/file02.php
+++ OSTAD/php and laravel/module05-Mastering on PHP File, Session, Date, Exception/practice/01/file02.php
@@ -1,5 +1,11 @@
 <?php
 $fileName = 'data/data.txt';
 $fp = fopen($fileName,'r');
-
+while($line = fgets($fp)){
+    echo $line;
+}
+rewind($fp);
+while($line = fgets($fp)){
+    echo $line;
+}
 fclose($fp);
