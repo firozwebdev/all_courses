e49d7e5571feb6885799dd26a2772a2982bc102b
Sabuz <everythingkst@gmail.com>
2023-10-18 14:35:07 +0600

    file modified

---
+++ OSTAD/Data Science and Machine Learning/Module03/quiz.py
@@ -0,0 +1,10 @@
+class Calculation :
+
+    def add(self,a,b):
+        return a + b
+
+
+baba = Calculation()
+a = int(input("please enter a value: "))
+b = int(input("please another  value: "))
+print (baba.add(a,b))
