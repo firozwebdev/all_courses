e9a4199a8c20480f251173b364b2b67ee36c8a6a
Sabuz <everythingkst@gmail.com>
2023-10-24 12:56:34 +0600

    file added

--- OSTAD/php and laravel/module05-Mastering on PHP File, Session, Date, Exception/CodeMama.php
+++ OSTAD/php and laravel/module05-Mastering on PHP File, Session, Date, Exception/CodeMama.php
@@ -24,3 +24,20 @@ if (checkNumbers($a, $b)) {
 } else {
     echo "False";
 }
+
+/*
+ * fscanf(STDIN,"%s %s",$firstName,$lastName); //taking input from console or terminal or cmd
+
+    echo $lastName.', '.$firstName;
+
+ */
+
+/*
+ * Write a program which will take seconds as input and output into hour:minute.
+ */
+$seconds = fgets(STDIN);
+
+$hours = floor($seconds / 3600); // 1 hour = 3600 seconds
+$minutes = floor(($seconds % 3600) / 60); // 1 minute = 60 seconds
+
+print sprintf("%02d:%02d", $hours, $minutes);
