036814ed81bab6098220574c34f33a8059784128
Sabuz <everythingkst@gmail.com>
2023-10-30 14:17:44 +0600

    file added

--- OSTAD/php and laravel/module05-Mastering on PHP File, Session, Date, Exception/Module 5/assignment 02/login.php
+++ OSTAD/php and laravel/module05-Mastering on PHP File, Session, Date, Exception/Module 5/assignment 02/login.php
@@ -1,3 +1,47 @@
+<?php
+
+session_start();
+if (isset($_POST["signup"])){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $data = [];
+        $data['email'] = $_POST["email"];
+        $data['role']  = $_POST['role'];
+        if($_POST['password'] == $_POST['check_password']){
+            $data['password']  = $_POST["password"];
+        }else{
+            echo "Invalid email or password.";
+        }
+
+        print_r($data);
+
+        // Read user data from the file (users.json)
+//        $users = file("users.json", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+//
+//        foreach ($users as $user) {
+//            $userData = json_decode($user, true);
+//            if ($userData["email"] === $email && password_verify($password, $userData["password"])) {
+//                $_SESSION["user"] = $userData;
+//                header("Location: dashboard.php");
+//                exit();
+//            }
+//        }
+//        echo "Invalid email or password.";
+    }
+}
+
+if (isset($_POST["login"])){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $data = [];
+        $data['email'] = $_POST["email"];
+        $data['password'] = $_POST["password"];
+
+        print_r($data);
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <!-- Created By CodingNepal -->
 <html lang="en" dir="ltr">
@@ -19,34 +63,34 @@
     </div>
     <div class="form-container">
         <div class="slide-controls">
-            <input type="radio" name="slide" id="login">
-            <input type="radio" name="slide" id="signup" checked>
+            <input type="radio" name="slide" id="login" checked>
+            <input type="radio" name="slide" id="signup" >
             <label for="login" class="slide login">Login</label>
             <label for="signup" class="slide signup">Signup</label>
             <div class="slider-tab"></div>
         </div>
         <div class="form-inner">
-            <form action="#" class="login">
+            <form action="#" class="login" method="POST">
                 <div class="field">
-                    <input type="text" placeholder="Email Address" required>
+                    <input name="email" type="text" placeholder="Email Address" required>
                 </div>
                 <div class="field">
-                    <input type="password" placeholder="Password" required>
+                    <input name="password" type="password" placeholder="Password" required>
                 </div>
                 <div class="pass-link">
                     <a href="#">Forgot password?</a>
                 </div>
                 <div class="field btn">
                     <div class="btn-layer"></div>
-                    <input type="submit" value="Login">
+                    <input name="login" type="submit" value="Login">
                 </div>
                 <div class="signup-link">
                     Not a member? <a href="">Signup now</a>
                 </div>
             </form>
-            <form action="#" class="signup">
+            <form action="#" class="signup" method="POST">
                 <div class="field">
-                    <input type="text" placeholder="Email Address" required>
+                    <input name="email" type="text" placeholder="Email Address" required>
                 </div>
                 <div class="field">
                     <select name="role" id="">
@@ -57,14 +101,14 @@
                     </select>
                 </div>
                 <div class="field">
-                    <input type="password" placeholder="Password" required>
+                    <input name="password" type="password" placeholder="Password" required>
                 </div>
                 <div class="field">
-                    <input type="password" placeholder="Confirm password" required>
+                    <input name="check_password" type="password" placeholder="Confirm password" required>
                 </div>
                 <div class="field btn">
                     <div class="btn-layer"></div>
-                    <input type="submit" value="Signup">
+                    <input type="submit" name="signup" value="Signup">
                 </div>
             </form>
         </div>
