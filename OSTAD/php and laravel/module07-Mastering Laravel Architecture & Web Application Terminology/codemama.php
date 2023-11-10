e86786ec378f24bd3931d98fa72b423f09f96ce2
Sabuz <everythingkst@gmail.com>
2023-11-10 20:50:50 +0600

    file added

--- OSTAD/php and laravel/module07-Mastering Laravel Architecture & Web Application Terminology/codemama.php
+++ OSTAD/php and laravel/module07-Mastering Laravel Architecture & Web Application Terminology/codemama.php
@@ -1,22 +1,35 @@
 <?php
 
+//function countSubstrOccurrences($haystack, $needle) {
+//    $count = 0;
+//    $position = strpos($haystack, $needle);
+//
+//    while ($position !== false) {
+//        $count++;
+//        $position = strpos($haystack, $needle, $position + strlen($needle));
+//    }
+//
+//    return $count;
+//}
 function countSubstrOccurrences($haystack, $needle) {
     $count = 0;
-    $offset = 0;
+    $position = 0;
 
-    while (($pos = strpos($haystack, $needle, $offset)) !== false) {
+    while (($position = strpos($haystack, $needle, $position)) !== false) {
         $count++;
-        $offset = $pos + strlen($needle);
+        $position++;
     }
 
     return $count;
 }
-
+//fscanf(STDIN,"%s %s",$mainString,$subString);
 // Example usage
-$mainString = "abababab";
-$subString = "ab";
+$mainString = "aaaaaaa";
+$subString = "aa";
 
 $result = countSubstrOccurrences($mainString, $subString);
 
-echo $result;
-?>
+echo "The substring '$subString' appears $result times in the main string '$mainString'.";
+
+
+//fscanf(STDIN,"%s %s", $first,$second);
--- OSTAD/php and laravel/module07-Mastering Laravel Architecture & Web Application Terminology/codemama2.php
+++ OSTAD/php and laravel/module07-Mastering Laravel Architecture & Web Application Terminology/codemama2.php
@@ -22,4 +22,32 @@ if (isRotation($string1, $string2)) {
     echo "False";
 }
 
+/*
+ * <?php
+
+function isRotation($str1, $str2) {
+    // Check if the lengths of both strings are the same
+    if (strlen($str1) != strlen($str2)) {
+        return false;
+    }
+
+    // Concatenate str1 with itself and check if str2 is a substring
+    $concatenatedStr1 = $str1 . $str1;
+
+    return strpos($concatenatedStr1, $str2) !== false;
+}
+
+// Example usage
+fscanf(STDIN,"%s %s",$string1,$string2);
+
+if (isRotation($string1, $string2)) {
+    echo "True";
+} else {
+    echo "False";
+}
+
+
+
+ */
+
 
