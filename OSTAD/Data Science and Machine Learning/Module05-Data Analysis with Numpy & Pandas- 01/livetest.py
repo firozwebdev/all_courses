cd9531047c1e8406a4975a12c72e29b1a9094045
Sabuz <everythingkst@gmail.com>
2023-11-13 20:49:24 +0600

    file added

--- OSTAD/Data Science and Machine Learning/Module05-Data Analysis with Numpy & Pandas- 01/livetest.py
+++ OSTAD/Data Science and Machine Learning/Module05-Data Analysis with Numpy & Pandas- 01/livetest.py
@@ -7,20 +7,30 @@ Then perform the operation, array % 5 == 0 """
 import numpy as np
 
 # Generate an array of 100 linearly spaced points between 50 and 60
-linear_array = np.linspace(50, 60, 100)
+# linear_array = np.linspace(50, 60, 100)
+#
+# # Convert the linear array to a matrix array of size (10, 10)
+# matrix_array = linear_array.reshape((10, 10))
+#
+# # Perform the operation array % 5 == 0
+# result_array = matrix_array % 5 == 0
+#
+# # Display the results
+# print("Linear Array:")
+# print(linear_array)
+#
+# print("\nMatrix Array:")
+# print(matrix_array)
+#
+# print("\nResult Array (array % 5 == 0):")
+# print(result_array)
 
-# Convert the linear array to a matrix array of size (10, 10)
-matrix_array = linear_array.reshape((10, 10))
+import numpy as np
 
-# Perform the operation array % 5 == 0
-result_array = matrix_array % 5 == 0
+# Create a 1D array from 100 to 0 with step -1
+arr_1d = np.arange(100, 0, -1)
 
-# Display the results
-print("Linear Array:")
-print(linear_array)
+# Reshape the 1D array to a 2D array of size (10, 10)
+arr_2d = arr_1d.reshape(10,10)
 
-print("\nMatrix Array:")
-print(matrix_array)
-
-print("\nResult Array (array % 5 == 0):")
-print(result_array)
+print(arr_2d)
