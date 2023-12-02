1b5d7d4c3457b72467f67edf499571d1b1286a19
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-12-03 01:22:20 +0600

    file added

---
+++ OSTAD/Cracking Coding Interview/Module01/practice/test.py
@@ -0,0 +1,14 @@
+def sum_of_min_max(nums):
+    max_n = nums[0]
+    min_n = nums[0]
+    
+    for i in range(len(nums)):
+        if nums[i] > max_n:
+            max_n = nums[i]
+        elif nums[i] < min_n:
+            min_n = nums[i]
+    return max_n + min_n
+
+
+sum = sum_of_min_max([1,56,34,12])
+print(sum)
