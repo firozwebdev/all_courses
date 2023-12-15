60621fbf65caff3bfeb4d612f5e27a366d63bfe8
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-12-15 12:40:22 +0600

    file added

--- OSTAD/Mastering Vue.js/module02/vite-project/main.js
+++ OSTAD/Mastering Vue.js/module02/vite-project/main.js
@@ -21,6 +21,7 @@ const add3 = (n1, n2) => n1 + n2;
 const data = add2(3, 4);
 //console.log(data);
 function sum2(n) {
+  //const sum5 = (n) => n.reduce((o, n) => o + n);
   const summation = n.reduce(function (o, n) {
     return o + n;
   });
@@ -38,3 +39,30 @@ const sum_4 = sum4([2, 3, 4, 5, 6, 7, 8, 9, 10]);
 const sum5 = (n) => n.reduce((o, n) => o + n);
 
 console.log(sum5([2, 3, 40, 5, 65, 7, 8, 9, 10]));
+
+function revString(str) {
+  return str.split("").reverse().join("");
+}
+
+//console.log(revString("hello world"));
+
+const revString2 = (str) => str.split("").reverse().join("");
+//console.log(revString2("I am sabuz"));
+
+const revString3 = (str) => str.split("").reverse().join("");
+//console.log(revString3("I am Baba"));
+
+const data2 = [12, 13, 14, 15];
+const [a, b, ...c] = data2;
+
+const summation = (args) => args.reduce((o, n) => o + n);
+//console.log(summation(c));
+
+const person = {
+  fname: "John",
+  lname: "Doe",
+  age: 34,
+};
+
+const { fname, ...rest } = person;
+console.log(fname, rest);
