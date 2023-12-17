876bdd665c9a5fb688f7e1a6e91215cf586f6aba
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-12-18 02:26:00 +0600

    file added

--- OSTAD/Cracking Coding Interview/Module03-String/practice/practice/main.js
+++ OSTAD/Cracking Coding Interview/Module03-String/practice/practice/main.js
@@ -1,21 +1,21 @@
-// const addBinary = (a, b) => {
-//   let i = a.length - 1;
-//   let j = b.length - 1;
-//   let carry = 0;
-//   let result = "";
+const addBinary = (a, b) => {
+  let i = a.length - 1;
+  let j = b.length - 1;
+  let carry = 0;
+  let result = "";
 
-//   while (i >= 0 || j >= 0) {
-//     const bit1 = i < 0 ? 0 : parseInt(a[i--]);
-//     const bit2 = j < 0 ? 0 : parseInt(b[j--]);
-//     const sum = bit1 + bit2 + carry;
-//     result = (sum % 2) + result;
-//     carry = Math.floor(sum / 2);
-//   }
-//   if (carry > 0) {
-//     result = carry + result;
-//   }
-//   return result;
-// };
+  while (i >= 0 || j >= 0) {
+    const bit1 = i < 0 ? 0 : parseInt(a[i--]);
+    const bit2 = j < 0 ? 0 : parseInt(b[j--]);
+    const sum = bit1 + bit2 + carry;
+    result = (sum % 2) + result;
+    carry = Math.floor(sum / 2);
+  }
+  if (carry > 0) {
+    result = carry + result;
+  }
+  return result;
+};
 
 //console.log(addBinary("1000", "101111"));
 
@@ -32,4 +32,27 @@ function findNumberInString(intputSting) {
 }
 
 const getfindNumberInString = findNumberInString("4ell25sd6f8");
-console.log(getfindNumberInString);
+//console.log(getfindNumberInString);
+
+const findNeedle = (haystack, needle) => {
+  let indices = [];
+  for (let i = 0; i <= haystack.length - needle.length; i++) {
+    //console.log(haystack.substring(i, i + needle.length));
+    if (haystack.substring(i, i + needle.length) === needle) {
+      indices.push(i);
+    }
+  }
+  //console.log(indices);
+  if (indices.length > 0) {
+    console.log(indices.join(", "));
+  } else {
+    console.log("No indices found");
+  }
+};
+
+const haystack =
+  "Hello, this is a sample haystack. This sample has another sample.";
+const needle = "sample";
+
+findNeedle(haystack, needle);
+//console.log(findNeedle("helloWorld", "hello"));
