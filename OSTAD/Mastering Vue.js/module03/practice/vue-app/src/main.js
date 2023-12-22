57575851fdcec0045aee427ef0a7f039863aca50
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-12-22 18:55:03 +0600

    file added

--- OSTAD/Mastering Vue.js/module03/practice/vue-app/src/main.js
+++ OSTAD/Mastering Vue.js/module03/practice/vue-app/src/main.js
@@ -3,6 +3,6 @@ import "./style.css";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
-const parent = document.getElementById("app");
-parent.insertAdjacentHTML("afterend", "<div class='app2'></div>");
-createApp(App).mount(".app2");
+// const parent = document.getElementById("app");
+// parent.insertAdjacentHTML("afterend", "<div class='app2'></div>");
+// createApp(App).mount(".app2");
