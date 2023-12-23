119c2c224b9079019454fbf19cd857993402297b
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-12-23 20:50:42 +0600

    file added

--- OSTAD/Cracking Coding Interview/Module04-Recursion/practice/recursion/recursion.js
+++ OSTAD/Cracking Coding Interview/Module04-Recursion/practice/recursion/recursion.js
@@ -1,18 +1,29 @@
 //checking palindrome
-function checkPalindrome(s) {
-  const pattern = "[^A-Za-z0-9]";
-  s = s.replace(pattern, "");
-  s = s.toLowerCase();
-  let i = 0;
-  let j = s.length - 1;
-  while (i < j) {
-    if (s[i] != s[j]) {
-      return false;
-    }
-    i++;
-    j--;
+
+function checkPalindrome(s, i, j) {
+  if (i >= j) {
     return true;
   }
+  if (s[i] != s[j]) {
+    return false;
+  }
+  return checkPalindrome(s, i + 1, j - 1);
+}
+function checkPal(s) {
+  const pattern = "[^A-Za-z0-9]";
+  s = s.replace(pattern, "").toLowerCase();
+  let i = 0;
+  let j = s.length - 1;
+  //   while (i < j) {
+  //     if (s[i] != s[j]) {
+  //       return false;
+  //     }
+  //     i++;
+  //     j--;
+  //     return true;
+  //   }
+  //using recursion method
+  return checkPalindrome(s, i, j);
 }
 
-console.log(checkPalindrome("hello world"));
+console.log(checkPal("peepholes"));
