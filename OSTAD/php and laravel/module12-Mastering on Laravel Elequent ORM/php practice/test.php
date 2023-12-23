27ab35b3a59b242c67f64459def98b5808b0d38f
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-12-24 02:00:42 +0600

    file added

--- OSTAD/php and laravel/module12-Mastering on Laravel Elequent ORM/php practice/test.php
+++ OSTAD/php and laravel/module12-Mastering on Laravel Elequent ORM/php practice/test.php
@@ -1,26 +1,16 @@
 <?php
-
-// $a = [2, 3, 4, 5, 6, 7];
-// foreach ($a as $key=>$val) {
-//     echo $key . ': ' . $val ."\n";
-// };
-function checkPalidrom($str,$i,$j) {
-    if($i>=$j){
-        return true;
+class Test{
+    public $name;
+    public $age;
+    public function __construct($name, $age){
+        $this->name = $name;
+        $this->age = $age;
     }
-    if($str[$i]!=$str[$j]){
-        return false;
-    }
-    return checkPalidrom($str,$i+1,$j-1);
-}
-function checkpal($str) {
-    //$pattern = (['^a-zA-Z0-9']);
-    $str = strtolower($str);
-    $i = 0;
-    $j = strlen($str)-1;
-    return checkPalidrom($str, $i, $j);
 
+    public function getName(){
+        echo "My name is {$this->name} and age is {$this->age}";
+    }
 }
 
-$output = checkpal("peep");
-echo $output ? "True" : "False";
+$sabuz = new Test("Sabuz",34);
+$sabuz->getName();
