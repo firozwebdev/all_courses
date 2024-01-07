b59f5f7303853e5a38342041dfaeac43471d3ecf
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-01-08 02:19:35 +0600

    file added

--- OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/Output.php
+++ OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/Output.php
@@ -2,6 +2,7 @@
 namespace App/Http/Controllers;
 use Illuminate/Http/Request;
 class HelloController extends Controller
+{
   /**
   * Display a listing of the resource.
   */
@@ -9,5 +10,18 @@ class HelloController extends Controller
   {
    echo "Method name is index";
   } 
-{
+  /**
+  * Display a listing of the resource.
+  */
+  public function create() 
+  {
+   echo "Method name is create";
+  } 
+  /**
+  * Show the form for editing the specified resource.
+  */
+  public function edit($id) 
+  {
+   echo "Method name is edit";
+  } 
 }
--- OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/method.js
+++ OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/method.js
@@ -1,61 +1,76 @@
-function methodMaker(methods){
-    let mi = ''
-    let mt = ''
-    const methodData = []
-   
-    for (let index = 0;  methods.length; index++) {
-        if(methods[index] == 'index'){
-           mt = mt.concat("  /**\n")
-           mt = mt.concat("  * Display a listing of the resource.\n")
-           mt = mt.concat("  */\n")
-           mt = mt.concat(`  public function ${methods[index]}() \n`)
-           mt = mt.concat("  {\n")
-           mt = mt.concat(`   echo \"Method name is ${methods[index]}\";`)
-           //mt += mi
-           mt = mt.concat("\n  } \n")
-           mi = ''
-           methodData.push(mt)
-          
-        } 
-       
+function callIndex() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Display a listing of the resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function index() \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is index\";`);
+  //mt += mi
+  mt = mt.concat("\n  } \n");
+  mi = "";
+  return mt;
+}
 
-        if(methods[index] == 'create'){
-           mt = mt.concat("  /**\n")
-           mt = mt.concat("  * Display a listing of the resource.\n")
-           mt = mt.concat("  */\n")
-           mt = mt.concat(`  public function ${methods[index]}() \n`)
-           mt = mt.concat("  {\n")
-           mt = mt.concat(`   echo \"Method name is ${methods[index]}\";`)
-           //mt += mi
-           mt = mt.concat("\n  } \n")
-           mi = ''
-           methodData.push(mt)
-         
-        }
-        return methodData
+function callCreate() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Display a listing of the resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function create() \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is create\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+function callEdit() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Show the form for editing the specified resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function edit($id) \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is edit\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+
+function makeController(controllerName, methods) {
+  const fs = require("fs");
+  const mt = [];
+  let data = "<?php\n";
+  data = data.concat(
+    "namespace App/Http/Controllers;\nuse Illuminate/Http/Request;"
+  );
+  data = data.concat(`\nclass ${controllerName} extends Controller\n`);
+  data = data.concat("{\n");
+  methods.forEach((element) => {
+    if (element == "index") {
+      data = data.concat(callIndex());
     }
+    if (element == "create") {
+      data = data.concat(callCreate());
+    }
+    if (element == "edit") {
+      data = data.concat(callEdit());
+    }
+  });
+
+  //console.log(data);
+  data = data.concat("}\n");
+
+  fs.writeFile("Output.php", data, (err) => {
+    // In case of a error throw err.
+    if (err) throw err;
+    console.log("working fine");
+  });
 }
 
-function makeController(controllerName,methods){
-    const fs = require('fs')
-    const mt = []
-    let data = "<?php\n"
-    data = data.concat("namespace App/\Http/\Controllers;\nuse Illuminate/\Http/\Request;")
-    data  = data.concat(`\nclass ${controllerName} extends Controller\n`)
-   // mt.push(methodMaker(methods)); 
-     data = data.concat(methodMaker(methods))
-     console.log(data)
-    data  = data.concat("{\n") 
-    data  = data.concat()  
-    data  = data.concat("}\n")   
-    
-    fs.writeFile('Output.php', data, (err) => {
-     
-        // In case of a error throw err.
-        if (err) throw err;
-        console.log('working fine');
-    })
-}
-
-makeController("HelloController",['index','create','edit','store','update','destroy'])
-
+makeController("HelloController", [
+  "index",
+  "create",
+  "edit",
+  "store",
+  "update",
+  "destroy",
+]);
