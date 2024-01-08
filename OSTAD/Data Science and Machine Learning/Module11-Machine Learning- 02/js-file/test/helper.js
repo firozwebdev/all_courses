c18cbe812b39c9c2aa5d8506ed7f2c6ca1228ecc
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-01-09 03:15:08 +0600

    file added

--- OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/js-file/test/helper.js
+++ OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/js-file/test/helper.js
@@ -1,60 +1,87 @@
-var s = 'HelloWorld'
-// for (let index = 0; index < s.length; index++) {
-//     const element = s[index];
-//     //console.log(element)
-    
+// var s = 'HelloWorld'
+// // for (let index = 0; index < s.length; index++) {
+// //     const element = s[index];
+// //     //console.log(element)
+
+// // }
+// // s = s.replace(/([A-Z])/g, '(\)').trim()
+// // console.log(s)
+// function test(){
+//     var strings = 'AppHttpControllers';
+//     var i=1;
+//     var character='';
+//     let result = ''
+//     while (i <= strings.length){
+//         character = strings.charAt(i);
+//         if (!isNaN(character * 1)){
+//             console.log('character is numeric');
+//         }else{
+//             if (character == character.toUpperCase()) {
+//                 result += '\\'+character
+
+//             }
+//             if (character == character.toLowerCase()){
+//                 result += character
+//             }
+
+//             //console.log(result)
+
+//         }
+//         i++;
+
+//     }
+
 // }
-// s = s.replace(/([A-Z])/g, '(\)').trim()
-// console.log(s)
-function test(){
-    var strings = 'AppHttpControllers';
-    var i=1;
-    var character='';
-    let result = ''
-    while (i <= strings.length){
-        character = strings.charAt(i);
-        if (!isNaN(character * 1)){
-            console.log('character is numeric');
-        }else{
-            if (character == character.toUpperCase()) {
-                result += '\\'+character
-                
-            }
-            if (character == character.toLowerCase()){
-                result += character
-            }
+// //test()
 
-            //console.log(result)
-                
-        }
-        i++;
-       
-    }
-   
+//     //console.log("'" + data.join("', '") + "'");
+//     function migrationFileName() {
+//         const now = new Date();
+
+//         const year = now.getFullYear();
+//         const month = String(now.getMonth() + 1).padStart(2, '0');
+//         const day = String(now.getDate()).padStart(2, '0');
+//         const hours = String(now.getHours()).padStart(2, '0');
+//         const minutes = String(now.getMinutes()).padStart(2, '0');
+//         const seconds = String(now.getSeconds()).padStart(2, '0');
+
+//         const formattedDateTime = `${year}_${month}_${day}_${hours}${minutes}${seconds}`;
+
+//         return formattedDateTime;
+//     }
+
+//   console.log(table_name('customer'))
+
+// seperate two strings
+function seperateStrings(arr) {
+  //console.log(arr);
+  let columns = [];
+  let column = {
+    name: "",
+    type: "",
+  };
+
+  arr.forEach((element) => {
+    //console.log(element);
+    let data = element.split(":");
+
+    //columns.push(columns);
+
+    //console.log(columns);
+
+    columns.push({
+      name: data[0],
+      type: data[1],
+    });
+  });
+  return columns;
 }
-//test()
-
-
-    //console.log("'" + data.join("', '") + "'");
-    function migrationFileName() {
-        const now = new Date();
-        
-        const year = now.getFullYear();
-        const month = String(now.getMonth() + 1).padStart(2, '0');
-        const day = String(now.getDate()).padStart(2, '0');
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-        const seconds = String(now.getSeconds()).padStart(2, '0');
-    
-        const formattedDateTime = `${year}_${month}_${day}_${hours}${minutes}${seconds}`;
-        
-        return formattedDateTime;
-    }
-    
-
-  console.log(table_name('customer'))
-
-
-
-
-
+console.log(
+  seperateStrings([
+    "name:string",
+    "email:string",
+    "address:string",
+    "age:string",
+  ])
+);
+//seperateStrings(["name:string", "email:string"]);
