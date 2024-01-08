ac9082e2dadde6b9392d142e47c4e3b3ac7d2554
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-01-08 13:17:01 +0600

    file added

---
+++ OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/js-file/maincode.js
@@ -0,0 +1,52 @@
+import { index, create, edit, store, update, destroy } from "./modules/methods";
+function makeController(controllerName, methods) {
+  const fs = require("fs");
+  const mt = [];
+  let data = "<?php\n";
+  data = data.concat(
+    "namespace App/Http/Controllers;\nuse Illuminate/Http/Request;"
+  );
+  data = data.concat(`\nclass ${controllerName} extends Controller\n`);
+  data = data.concat("{\n");
+  methods.forEach((element) => {
+    if (element == "index") {
+      data = data.concat(index());
+    }
+    if (element == "create") {
+      data = data.concat(create());
+    }
+    if (element == "store") {
+      data = data.concat(store());
+    }
+    if (element == "edit") {
+      data = data.concat(edit());
+    }
+    if (element == "show") {
+      data = data.concat(show());
+    }
+    if (element == "update") {
+      data = data.concat(update());
+    }
+    if (element == "destroy") {
+      data = data.concat(destroy());
+    }
+  });
+
+  //console.log(data);
+  data = data.concat("}\n");
+
+  fs.writeFile(controllerName + ".php", data, (err) => {
+    // In case of a error throw err.
+    if (err) throw err;
+    console.log("working fine");
+  });
+}
+
+makeController("HelloController", [
+  "index",
+  "create",
+  "edit",
+  "store",
+  "update",
+  "destroy",
+]);
---
+++ OSTAD/Data Science and Machine Learning/Module11-Machine Learning- 02/js-file/modules/methods.js
@@ -0,0 +1,80 @@
+export function index() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Display a listing of the resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function index() \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is index\";`);
+  //mt += mi
+  mt = mt.concat("\n  } \n");
+  mi = "";
+  return mt;
+}
+
+export function create() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Display a listing of the resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function create() \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is create\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+export function store() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Store a newly created resource in storage.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function store(Request $request) \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is store\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+export function update() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Update the specified resource in storage.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function update(Request $request, string $id) \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is update\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+export function edit() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Show the form for editing the specified resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function edit(string $id) \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is edit\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+export function show() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Display the specified resource.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function show(string $id) \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is show\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
+export function destroy() {
+  mt = "";
+  mt = mt.concat("  /**\n");
+  mt = mt.concat("  * Remove the specified resource from storage.\n");
+  mt = mt.concat("  */\n");
+  mt = mt.concat(`  public function destroy(string $id) \n`);
+  mt = mt.concat("  {\n");
+  mt = mt.concat(`   echo \"Method name is destroy\";`);
+  mt = mt.concat("\n  } \n");
+  return mt;
+}
