3f141d7b6a1c00c8aefcdeea1873e6c93f987dd4
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-01-21 01:12:03 +0600

    file added

--- OSTAD/Cracking Coding Interview/Module08-sorting/binary_search.js
+++ OSTAD/Cracking Coding Interview/Module08-sorting/binary_search.js
@@ -1,22 +1,41 @@
+// function binary_search(arr, target) {
+//   let left = 0;
+//   let right = arr.length - 1;
+//   //console.log("hello ");
+
+//   while (left <= right) {
+//     let mid = Math.floor(left + (right - left) / 2);
+//     //console.log(mid);
+//     if (target == arr[mid]) {
+//       return mid;
+//     }
+//     if (target < arr[mid]) {
+//       right = mid - 1;
+//     } else {
+//       left = mid + 1;
+//     }
+//   }
+// }
+
+// console.log(
+//   binary_search([2, 5, 6, 7, 8, 70, 80, 90, 100, 101, 102, 103, 104, 105], 100)
+// );
+
 function binary_search(arr, target) {
   let left = 0;
   let right = arr.length - 1;
-  //console.log("hello ");
 
   while (left <= right) {
     let mid = Math.floor(left + (right - left) / 2);
-    //console.log(mid);
     if (target == arr[mid]) {
       return mid;
     }
-    if (target < arr[mid]) {
-      right = mid - 1;
-    } else {
+    if (arr[mid] < target) {
       left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
 }
 
-console.log(
-  binary_search([2, 5, 6, 7, 8, 70, 80, 90, 100, 101, 102, 103, 104, 105], 100)
-);
+console.log(binary_search([2, 5, 6, 7, 8, 70, 80], 7));
