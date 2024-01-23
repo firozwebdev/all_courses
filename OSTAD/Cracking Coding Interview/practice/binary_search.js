03c6c6f2ee213d20caab02123b42b01d5c86ead0
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-01-23 11:59:14 +0600

    file added

--- OSTAD/Cracking Coding Interview/practice/binary_search.js
+++ OSTAD/Cracking Coding Interview/practice/binary_search.js
@@ -1,30 +1,69 @@
+// function test(arr, target) {
+//   let length = arr.length;
+//   let sortedArray = [];
+//   for (let i = 0; i < length; i++) {
+//     sortedArray[i] = [arr[i], i];
+//   }
+
+//   sortedArray.sort((a, b) => {
+//     return a[0] - b[0];
+//   });
+//   for (let i = 0; i < sortedArray.length; i++) {
+//     let n1 = sortedArray[i][0];
+//     let n2 = target - sortedArray[i][0];
+//     let index = binar_search(sortedArray, i + 1, n2);
+//     if (index !== -1) {
+//       return [sortedArray[i][1], index];
+//     }
+//   }
+// }
+
+// function binar_search(arr, start, target) {
+//   let end = arr.length - 1;
+//   while (start <= end) {
+//     let mid = Math.floor(start + (end - start) / 2);
+//     if (target == arr[mid][0]) {
+//       return arr[mid][1];
+//     }
+//     if (target < arr[mid][0]) {
+//       end = mid - 1;
+//     } else {
+//       start = mid + 1;
+//     }
+//   }
+// }
+
+//same function implementation
 function test(arr, target) {
-  let length = arr.length;
   let sortedArray = [];
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     sortedArray[i] = [arr[i], i];
   }
 
-  sortedArray.sort((a, b) => {
+  sortedArray.sort(function (a, b) {
     return a[0] - b[0];
   });
+
   for (let i = 0; i < sortedArray.length; i++) {
     let n1 = sortedArray[i][0];
-    let n2 = target - sortedArray[i][0];
-    let index = binar_search(sortedArray, i + 1, n2);
-    if (index !== -1) {
+    let n2 = target - n1;
+    let index = binary_search(sortedArray, i + 1, n2);
+    if (index != -1) {
       return [sortedArray[i][1], index];
     }
   }
+
+  return -1;
 }
 
-function binar_search(arr, start, target) {
+function binary_search(arr, start, target) {
   let end = arr.length - 1;
   while (start <= end) {
     let mid = Math.floor(start + (end - start) / 2);
     if (target == arr[mid][0]) {
       return arr[mid][1];
     }
+
     if (target < arr[mid][0]) {
       end = mid - 1;
     } else {
