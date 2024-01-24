7d4dab80893d2506c9ab5fada41969810031d8b0
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-01-24 11:55:33 +0600

    file added

--- OSTAD/Cracking Coding Interview/practice/maxsum_array.js
+++ OSTAD/Cracking Coding Interview/practice/maxsum_array.js
@@ -12,6 +12,21 @@
 //   return max_sum;
 // }
 
+// function maxsum(arr, B) {
+//   let max_sum = 0;
+//   let current_sum = 0;
+//   for (let i = 0; i < B; i++) {
+//     current_sum += arr[i];
+//   }
+//   max_sum = current_sum;
+
+//   for (let i = B - 1, j = arr.length - 1; i >= 0; i--, j--) {
+//     current_sum = current_sum - arr[i] + arr[j];
+//     max_sum = Math.max(max_sum, current_sum);
+//   }
+//   return max_sum;
+// }
+
 function maxsum(arr, B) {
   let max_sum = 0;
   let current_sum = 0;
@@ -21,10 +36,9 @@ function maxsum(arr, B) {
   max_sum = current_sum;
 
   for (let i = B - 1, j = arr.length - 1; i >= 0; i--, j--) {
-    current_sum = current_sum - arr[i] + arr[j];
+    current_sum = current_sum + arr[j] - arr[i];
     max_sum = Math.max(max_sum, current_sum);
   }
   return max_sum;
 }
-
 console.log(maxsum([5, -2, 3, 1, 2], 3));
