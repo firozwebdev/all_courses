23e816c4777acd7c78190a88f1cb25f3da3571ec
Sabuz <everythingkst@gmail.com>
2024-03-02 19:21:21 +0600

    leetcode problem solving

--- Leetcode/pickFromBothSide.js
+++ Leetcode/pickFromBothSide.js
@@ -17,19 +17,39 @@
 //     return max;
 // }
 
+// function pickFromBothSide(A,B){
+//     let sum = 0;
+//     for(let i=0;i<B;i++){
+//         sum += A[i]
+//     }
+//     max  = sum;
+   
+//     for(i = A.length-1, j = B-1; i > A.length - B, j >= 0; i--,j--){
+//         sum = sum - A[j] + A[i];
+//         max = Math.max(max,sum)
+//     }
+//     return max;
+
+// }
+
 function pickFromBothSide(A,B){
     let sum = 0;
-    for(let i=0;i<B;i++){
-        sum += A[i]
-    }
-    max  = sum;
-   
-    for(i = A.length-1, j = B-1; i > A.length - B, j >= 0; i--,j--){
-        sum = sum - A[j] + A[i];
-        max = Math.max(max,sum)
-    }
-    return max;
-
+        
+        for(let i = 0; i < B; i++){
+            sum += A[i]
+        }
+        
+        max = sum;
+        
+        for(let i = A.length-1; i < A.length - B; i--){
+            for(let j = B-1; j >= 0; j--){
+                sum = sum - A[j] + A[i];
+                max  = Math.max(max,sum)
+            }
+          
+        }
+        
+        return max;
 }
 
 
@@ -38,4 +58,11 @@ console.log(pickFromBothSide([5,-2,3,1,2],3))
 5-2+3 = 6
 5-2
 
+*/
+
+/*
+for(i = n-1, j = B-1; i > n - B, j >= 0; i--,j--){
+        sum = sum - A[j] + A[i];
+        max = Math.max(max,sum)
+    }
 */
