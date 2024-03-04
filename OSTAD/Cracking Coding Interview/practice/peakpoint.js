c3cea3424c5353143e6d49acb9fe1d3f6b2d993e
Firoz Rahman Sabuz <everythingkst@gmail.com>
2024-03-04 21:18:54 +0600

    recheck

--- OSTAD/Cracking Coding Interview/practice/peakpoint.js
+++ OSTAD/Cracking Coding Interview/practice/peakpoint.js
@@ -3,7 +3,7 @@ function peakPoint(arr) {
   let n = arr.length;
   for (let i = 0; i < n; i++) {
     if (isPeak(i, arr)) {
-      return arr[i];
+      return 1;
     }
   }
   return 0;
@@ -12,15 +12,20 @@ function peakPoint(arr) {
 function isPeak(i, arr) {
   // Your code here
   for (let j = 0; j < arr.length; j++) {
-    if (arr[j] <= arr[i]) {
+    if (arr[j] >= arr[i]) {
       return 1;
     }
   }
   for (let j = i + 1; j < arr.length; j++) {
-    if (arr[j] >= arr[i]) {
+    if (arr[j] <= arr[i]) {
       return 1;
     }
   }
   return 0;
 }
-console.log(peakPoint([2, 4, 5, 6, 8, 9]));
+console.log(
+  peakPoint([
+    17660, 7480, 26424, 26634, 10867, 7463, 27919, 12159, 18239, 21197, 12023,
+    1147, 32499, 28487,
+  ])
+);
