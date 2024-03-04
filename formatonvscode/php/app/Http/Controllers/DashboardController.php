d6533e80ced3dd7d8dcde99dc3105ffc46a42a55
Sabuz <everythingkst@gmail.com>
2024-03-04 15:47:39 +0600

    working on custom configuration file like extensions and settings

---
+++ formatonvscode/php/app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        return view('dashboard');
+    }
+
+    public function test(Request $request)
+    {
+        $name = $request->input('name');
+        return view('test', ['name' => $name]);
+    }
+}
---
+++ react/1.4/test/test.js
@@ -0,0 +1,9 @@
+
+function test() {
+  const fruits = ['apple', 'banana', 'orange'];
+  fruits.forEach((fruit, index) => {
+     console.log(index + 1 + ' is ' + fruit);
+  });
+}
+
+test();
